a959cf104f1576daa76a4a98f83213277aeed08d
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2025-06-04 15:30:50 +0700

    .

--- app/Http/Controllers/Dokter/JadwalPeriksaController.php
+++ app/Http/Controllers/Dokter/JadwalPeriksaController.php
@@ -36,20 +36,14 @@ class JadwalPeriksaController extends Controller
             'hari' => 'required|string|max:255',
             'jam_mulai' => 'required',
             'jam_selesai' => 'required',
-            'status' => 'required|in:aktif,nonaktif',
         ]);
-
-        if ($request->status === 'aktif') {
-            JadwalPeriksa::where('id_dokter', Auth::user()->id)
-                ->update(['status' => 'nonaktif']);
-        }
 
         JadwalPeriksa::create([
             'id_dokter' => Auth::user()->id,
             'hari' => $request->hari,
             'jam_mulai' => $request->jam_mulai,
             'jam_selesai' => $request->jam_selesai,
-            'status' => $request->status,
+            'status' => 'nonaktif',
         ]);
         
         return redirect()->route('dokter.jadwal-periksa.index')->with('status', 'jadwal-periksa-created');  
